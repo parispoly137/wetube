9ca8df1dc103e753e1608fd2ba56ab78e0586eb7
parispoly137 <tkdgns6578@naver.com>
2021-03-11 16:25:04 +0900

    #2.7 Express Core: Middlewares part Two
    - cookie-parser, body-parser, morgan, helmet 등의 middleware 설치

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import express from 'express';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "cookie-parser";
+
+
 const app = express()
 
 const PORT = 4000;
@@ -9,11 +15,11 @@ const handleHome = (req, res) => res.send("Hello from home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile!!");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-}
-app.use(betweenHome)
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 
