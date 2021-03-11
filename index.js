f0a460d21aeda355dbf76b41b508fa49c2079d62
parispoly137 <tkdgns6578@naver.com>
2021-03-11 14:21:49 +0900

    #2.6 Express Core: Middlewares
    - middleware의 기본 원리 및 정의
    - 전역적, 지역적 정의 ~ 순서 중요

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Test normal operation by changing the project folder to Linux");
+const handleHome = (req, res) => res.send("Hello from home!");
 
 const handleProfile = (req, res) => res.send("You are on my profile!!");
+
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+}
+app.use(betweenHome)
 
 app.get("/", handleHome);
 
