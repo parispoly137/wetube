a9d1811bd4a6fa4c0a9b81bdc5e0445ef26a5615
parispoly137 <tkdgns6578@naver.com>
2021-03-12 14:37:33 +0900

    #2.13 Installing Pug
    - pug 설치 및 express api 기본 설정
     ~ view engine을 pug로 설정
    - controller의 res.send 메소드를 res.render로 변경
    - rendering할 각 url에 해당하는 pug파일 생성

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -5,11 +5,11 @@ import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, home)
-globalRouter.get(routes.search, search)
-globalRouter.get(routes.join, join)
-globalRouter.get(routes.login, login)
-globalRouter.get(routes.logout, logout)
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 
 export default globalRouter;
