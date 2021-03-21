eec2e7a4194b7f158dcba20cef745ca856d04854
parispoly137 <tkdgns6578@naver.com>
2021-03-21 12:00:49 +0900

    #3.7 Uploading and Creating a Video part Two
    - 비디오 업로드 경로 재설정
      ~ middleware, app.js
    - static file 사용 및 문제점
    - MongoDB 컬렉션 확인 및 데이터 삭제

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(function (req, res, next) {
   return next();
 });
 
-
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
