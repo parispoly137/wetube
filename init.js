e52b732b87ab8b0d8bc855010cbc90614f7f1416
parispoly137 <tkdgns6578@naver.com>
2021-04-07 18:41:54 +0900

    #7.2 Local Authentication with Passport part Two
    - passport의 serializeUser 메서드 사용
     ~ 쿠키에 정보 담기
    - userController에서 (User.js에서 정의한) User 메서드를 이용하여 쿠키 생성
    - mongodb에 지정한 값이 저장되었는지 확인

--- init.js
+++ init.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 import app from "./app";
 import "./db";
 
-dotenv.config();
-
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
