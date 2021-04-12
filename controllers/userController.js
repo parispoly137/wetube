0bf701d228f6d3ec6080e21259c1cd6d0dfe5405
parispoly137 <tkdgns6578@naver.com>
2021-04-12 11:57:27 +0900

    #7.6 Github Log In part One
    - passport-github 설치
    - github 개발자 페이지에서 application 등록
     ~ github ID, secret 가져오기
    - passport.js에 GithubStrategy 코드 추가
      ~ LoginCallback 함수 선언
      ~ userController에서 해당 함수 정의

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,9 +38,15 @@ export const postLogin = passport.authenticate('local', {
   successRedirect: routes.home
 });
 
+
+
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 // Log out
 export const logout = (req, res) => {
-  // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 
--- passport.js
+++ passport.js
@@ -1,7 +1,20 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";  // 1) import
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
+
+passport.use(new GithubStrategy({   // 2)새로운 Strategy를 만든다.
+  clientID: process.env.GH_ID,
+  clientSecret: process.env.GH_SECRET,
+  callbackURL: "http://localhost:4000/auth/github/callback"
+}), githubLoginCallback);  // 4) 콜백 함수 추가
+
+// 3) 함수 선언 ... 사용자가 깃헙에서 돌아왔을 때 실행되는 함수 -> userController에서 
+
+// 5) 함수 선언 ... 깃헙으로 보내는 함수 -> userController에서 
+
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
